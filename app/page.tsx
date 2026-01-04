4993ae6f9a0b3868f26b3573ac0ef599e3100694
PRS Home Connect <prshometravel@gmail.com>
2026-01-03 21:27:39 -0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function home() {
+return (
+<main>
+<h1>PRS Home Connect</h1>
+<p>connecting Homeowners with trusted contractors.</p>
+</main>
+);
+}
+
---
+++ app/post-job/page.tsx
@@ -0,0 +1,8 @@
+export default function PostJobPage(){
+return( 
+<main style={{ padding: 24 }}>
+<h1>post a job</h1>
+<p>This is the post-job page.</p>
+</main>
+);
+}
